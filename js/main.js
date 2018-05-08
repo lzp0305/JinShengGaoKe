2dcc53c767147d31eb7de5976904f40e17b4f6e9
李志鹏 <lzp0305@gmail.com>
2018-05-08 11:04:49 +0800

    2018-5-8

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// header导航滑块
+function block() {
+	var oldLeft = $('.menu_on').position().left;
+	$('.block').css('left', oldLeft);
+};
+
+$(document).ready(function() {
+	block();
+	$('.fir_item').hover(function() {
+		var runtimeLeft = $(this).position().left;
+		$('.block').css('left', runtimeLeft);
+		$('.menu_on').children('.fir_link').css('color', '#444');
+		$(this).children('.fir_link').css('color', '#da251c');
+		$(this).find('.sec_item').each(function(index, el) {
+			$(el).css('transition-delay', '0.'+index+'s');
+		});
+	}, function() {
+		block();
+		$('.fir_link').css('color', '#444');
+		$('.menu_on').children('.fir_link').css('color', '#da251c');
+	});
+});
